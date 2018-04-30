1500193299c1c1e221f53d52b577760127447950
anhle <anhle@ttab.me>
2018-04-30 19:07:10 +0700

    update

---
+++ timehelper.go
@@ -0,0 +1,23 @@
+package timehelper
+
+import "time"
+
+//TimeNowUTC get time now UTC
+func TimeNowUTC() time.Time {
+	return time.Now().UTC()
+}
+
+// ConvertUnixToTimeUTC unix -> time
+func ConvertUnixToTimeUTC(unix int64) time.Time {
+	return time.Unix(unix, 0).UTC()
+}
+
+// ConvertTimeToString time.Time -> string
+func ConvertTimeToString(time time.Time) string {
+	return time.UTC().Format("2006-01-02T15:04:05.999999Z")
+}
+
+// ConvertStringToTime string -> time.Time
+func ConvertStringToTime(str string) (time.Time, error) {
+	return time.Parse("2006-01-02T15:04:05.999999Z", str)
+}
